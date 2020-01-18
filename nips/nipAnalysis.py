fcbafd8d50f5c853764114c2e41383391b098603
Muhammad Haseeb UR Rehman Khan <haseebulr@yahoo.com>
2020-01-18 17:23:58 +0900

    Code cleaning and modifying

--- nips/nipAnalysis.py
+++ nips/nipAnalysis.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Sat Nov 23 21:43:22 2019
+Updated on Sat Jan 18 16:27:22 2020
 
 @author: khan
 """
@@ -9,23 +9,28 @@ Created on Sat Nov 23 21:43:22 2019
 import pandas as pd
 import matplotlib.pyplot as plt
 import re
+from scipy.signal import savgol_filter
+from openpyxl import load_workbook
 
-TIME_SLICES = 30 # Max time slice number
-T = [i for i in range(31)] # Total time slices
+file_name = "60topics.xlsx"
+wb = load_workbook(file_name, read_only=True)
+TOPICS = int(file_name[:2])
+TIME_SLICES = 31
+T = [i for i in range(TIME_SLICES)] # Total time slices
 dtm_gl = 0 # global value: changed with graph() function
 year_col = 60 # year's column in sheet name "Theta"
 d_years = {} # count of docs in each year, filled with function Init_d_years() call
 
-def removeSpecialChar(orignal): 
+def removeSpecialChar(orignal):
     return re.sub(r"[^a-zA-Z0-9.]+", '', orignal)
 def strToval(a):
     w,m = a.split(",")
     return removeSpecialChar(w)
 def J(var): # use in graph()
-    return JS[var][dtm_gl*31:(dtm_gl+1)*31]
-def DTMWithAllLDA(a): # use in DTMcount(), can also be called indiviually 
-    return [i[1] for i in LDA_related_DTM if i[0] == a] 
-  
+    return JS[var][dtm_gl*TIME_SLICES:(dtm_gl+1)*TIME_SLICES]
+def DTMWithAllLDA(a): # use in DTMcount(), can also be called indiviually
+    return [i[1] for i in DTM_related_LDA if i[0] == a]
+
 def populationGraph(t): # graph of estimated number of docs for topic t of LDA
     d = {}
     for p in Theta_ls:
@@ -35,9 +40,16 @@ def populationGraph(t): # graph of estimated number of docs for topic t of LDA
         else:
             d[p[year_col]] += p[t]
             d_years[p[year_col]] += 1
-            
+
     sorted_d_keys = sorted(d.keys())
-    plt.plot([d[i]/d_years[i] for i in sorted_d_keys])
+    output = [d[i]/d_years[i] for i in sorted_d_keys]
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.plot(output, label = "Original")
+    y = savgol_filter(output, 15, 3)
+    ax.plot(y, label = "Smooth")
+    ax.legend()
+    plt.show()
 
 def Init_d_years(): #fill d_years dict
     for p in Theta_ls:
@@ -46,10 +58,9 @@ def Init_d_years(): #fill d_years dict
 
 def timeCorr(t1,t2):
     corr_time = []
-    Init_d_years() if not(d_years) else None
-    
+
     for i in sorted(d_years.keys()):
-        a = Theta_df[Theta_df[60]==i]
+        a = Theta_df[Theta_df[TOPICS]==i]
         corr_time.append(a[t1].corr(a[t2]))
     fig = plt.figure()
     ax = fig.add_subplot(111)
@@ -57,39 +68,37 @@ def timeCorr(t1,t2):
     ax.plot(corr_time)
 
 def highThetaCorr(t1, t2):
-    corr_time = []  
+    corr_time = []
     for i in sorted(d_years.keys()):
         a = Theta_df[Theta_df[60]==i]
         corr_time.append(a[a[t1]>.015][t1].corr(a[a[t2]>.015][t2]))
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
-    ax.plot(corr_time)
-    ax.legend()
-    plt.show()
+    plt.plot(corr_time)
 
-def graph(dtm, ls): #ls is list of LDA topics, 
+def graphJS(dtm, ls): #ls is list of LDA topics,
     global dtm_gl
     dtm_gl = dtm
     fig = plt.figure()
     ax = fig.add_subplot(111)
+    plt.ylabel("JSD value")
+    plt.xlabel("Time slot")
     for i in ls:
         ax.plot(T, J(i), label="Topic %i" %i)
     ax.legend()
     plt.show()
-    
+
 def getAllDTMwords(topic):
     words = []
-    for i in range(31):
+    for i in range(TIME_SLICES):
         for j in range(50):
-            w = strToval(DTM[i*TIME_SLICES+topic][j])
+            w = strToval(DTM[i*TOPICS+topic][j])
             if w not in words:
                 words.append(w)
     return words
 
 def DTMcount():
     DTM_count_dict = {}
-    
-    for i in LDA_related_DTM:
+
+    for i in DTM_related_LDA:
         if i[0] in DTM_count_dict.keys():
             DTM_count_dict[i[0]] += 1
         else:
@@ -98,8 +107,8 @@ def DTMcount():
     return DTM_count_dict, DTM_2orMore
 
 def getDTM(topic):
-    print([strToval(DTM[topic][i]) for i in range(50)])    
-def getLDA(topic):        
+    print([strToval(DTM[topic][i]) for i in range(50)])
+def getLDA(topic):
     print([strToval(LDA[topic][i]) for i in range(50)])
 
 def comp(l1, l2):
@@ -117,23 +126,26 @@ def common(l1, l2):
         if i in l2:
             print(i, end = ", ")
 
-f = pd.ExcelFile("60topics.xlsx")
+f = pd.ExcelFile(file_name)
 DTM = pd.read_excel(f, sheet_name = "DTM", index_col=0).values.tolist()
 LDA = pd.read_excel(f, sheet_name = "LDA", index_col=0).values.tolist()
 DTM_time = pd.read_excel(f, sheet_name = "DTMTime").values.tolist()
 LDA_time = pd.read_excel(f, sheet_name = "LDATime").values.tolist()
 JS = pd.read_excel(f, sheet_name = "JS", index_col=0).values.tolist()
-Theta_df = pd.read_excel(f, sheet_name = "Theta", index_col=0)
-Theta_ls = Theta_df.values.tolist()
+if 'Theta' in wb.sheetnames:
+    Theta_df = pd.read_excel(f, sheet_name = "Theta", index_col=0)
+    Theta_ls = Theta_df.values.tolist()
+if 'DTMDistribution' in wb.sheetnames:
+    DTM_gemma = pd.read_excel(f, sheet_name = "DTMDistribution", index_col=0)
 
-LDA_related_DTM = []
+DTM_related_LDA = []
 
 for i_index, i in enumerate(JS):
     for j_index, j in enumerate(i):
         if j<=0.7:
-            a = [j_index//31, i_index]
-            if a not in LDA_related_DTM:
-                LDA_related_DTM.append(a)
-LDA_related_DTM = sorted(LDA_related_DTM)
-Init_d_years() #function call to fill d_years dict    
+            a = [j_index//TIME_SLICES, i_index]
+            if a not in DTM_related_LDA:
+                DTM_related_LDA.append(a)
+DTM_related_LDA = sorted(DTM_related_LDA)
+Init_d_years() #function call to fill d_years dict
 DTM_all, DTM_2orMore = DTMcount()
--- tweets/GraphAnalysis.py
+++ tweets/GraphAnalysis.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Sat Nov 23 21:43:22 2019
+Updated on Sat Jan 18 16:04:22 2020
 
 @author: khan
 """
@@ -10,83 +10,39 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import re
 
-TIME_SLICES = 30
-T = [i for i in range(31)]
+file_name = "30topics.xlsx"
+TOPICS = int(file_name[:2])
+TIME_SLICES = 17
+T = [i for i in range(TIME_SLICES)]
 dtm_gl = 0
-year_col = 60
-d_years = {}
-
-def J(var):
-    return JS[var][dtm_gl*31:(dtm_gl+1)*31]
-def DTMWithAllLDA(a):
-    return [i[1] for i in LDA_related_DTM if i[0] == a]
-
-# Run fisrt for any topic atleast once
-def populationGraph(t):
-    d = {}
-    for p in Theta_ls:
-        if p[year_col] not in d.keys():
-            d[p[year_col]] = p[t]
-            d_years[p[year_col]] = 1
-        else:
-            d[p[year_col]] += p[t]
-            d_years[p[year_col]] += 1
-
-    sorted_d_keys = sorted(d.keys())
-    plt.plot([d[i]/d_years[i] for i in sorted_d_keys])
-
-def timeCorr(t1,t2):
-    corr_time = []
-    for i in sorted(d_years.keys()):
-        a = Theta_df[Theta_df[60]==i]
-        corr_time.append(a[t1].corr(a[t2]))
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
-    ax.set_ylim(-0.75,0.75)
-    ax.plot(corr_time)
-    fig.show()
-
-
-def graph(dtm, x, a=None, b=None, c=None, d=None, e=None, f=None, g=None, h=None, i=None, j=None):
-    global dtm_gl
-    dtm_gl = dtm
-    if j:
-        plt.plot(T,J(x), T,J(a), T,J(b), T,J(c), T,J(d), T,J(e), T,J(f), T,J(g), T,J(h), T,J(i), T,J(j))
-    elif i:
-        plt.plot(T,J(x), T,J(a), T,J(b), T,J(c), T,J(d), T,J(e), T,J(f), T,J(g), T,J(h), T,J(i))
-    elif h:
-        plt.plot(T,J(x), T,J(a), T,J(b), T,J(c), T,J(d), T,J(e), T,J(f), T,J(g), T,J(h))
-    elif g:
-        plt.plot(T,J(x), T,J(a), T,J(b), T,J(c), T,J(d), T,J(e), T,J(f), T,J(g))
-    elif f:
-        plt.plot(T,J(x), T,J(a), T,J(b), T,J(c), T,J(d), T,J(e), T,J(f))
-    elif e:
-        plt.plot(T,J(x), T,J(a), T,J(b), T,J(c), T,J(d), T,J(e))
-    elif d:
-        plt.plot(T,J(x), T,J(a), T,J(b), T,J(c), T,J(d))
-    elif c:
-        plt.plot(T,J(x), T,J(a), T,J(b), T,J(c))
-    elif b:
-        plt.plot(T,J(x), T,J(a), T,J(b))
-    elif a:
-        plt.plot(T,J(x), T,J(a))
-    else:
-        plt.plot(T,J(x))
 
 def removeSpecialChar(orignal):
-    final = re.sub(r"[^a-zA-Z0-9.]+", '', orignal)
-    return final
-
+    return re.sub(r"[^a-zA-Z0-9.]+", '', orignal)
 def strToval(a):
     w,m = a.split(",")
-    w = removeSpecialChar(w)
-    return w
+    return removeSpecialChar(w)
+def J(var):
+    return JS[var][dtm_gl*TIME_SLICES:(dtm_gl+1)*TIME_SLICES]
+def DTMWithAllLDA(a):
+    return [i[1] for i in DTM_related_LDA if i[0] == a]
+
+def graphJS(dtm, ls): #ls is list of LDA topics,
+    global dtm_gl
+    dtm_gl = dtm
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    plt.ylabel("JSD value")
+    plt.xlabel("Time slot")
+    for i in ls:
+        ax.plot(T, J(i), label="Topic %i" %i)
+    ax.legend()
+    plt.show()
 
 def getAllDTMwords(topic):
     words = []
-    for i in range(31):
+    for i in range(TIME_SLICES):
         for j in range(50):
-            w = strToval(DTM[i*TIME_SLICES+topic][j])
+            w = strToval(DTM[i*TOPICS+topic][j])
             if w not in words:
                 words.append(w)
     return words
@@ -94,7 +50,7 @@ def getAllDTMwords(topic):
 def DTMcount():
     DTM_count_dict = {}
 
-    for i in LDA_related_DTM:
+    for i in DTM_related_LDA:
         if i[0] in DTM_count_dict.keys():
             DTM_count_dict[i[0]] += 1
         else:
@@ -122,23 +78,21 @@ def common(l1, l2):
         if i in l2:
             print(i, end = ", ")
 
-f = pd.ExcelFile("60topics.xlsx")
+f = pd.ExcelFile(file_name)
 DTM = pd.read_excel(f, sheet_name = "DTM", index_col=0).values.tolist()
 LDA = pd.read_excel(f, sheet_name = "LDA", index_col=0).values.tolist()
-DTM_time = pd.read_excel(f, sheet_name = "DTMTime").values.tolist()
-LDA_time = pd.read_excel(f, sheet_name = "LDATime").values.tolist()
+DTM_time = pd.read_excel(f, sheet_name = "DTMTime",).values.tolist()[0][1]
+LDA_time = pd.read_excel(f, sheet_name = "LDATime").values.tolist()[0][1]
 JS = pd.read_excel(f, sheet_name = "JS", index_col=0).values.tolist()
-Theta_df = pd.read_excel(f, sheet_name = "Theta", index_col=0)
-Theta_ls = Theta_df.values.tolist()
 
-LDA_related_DTM = []
+DTM_related_LDA = []
 
 for i_index, i in enumerate(JS):
     for j_index, j in enumerate(i):
         if j<=0.7:
-            a = [j_index//31, i_index]
-            if a not in LDA_related_DTM:
-                LDA_related_DTM.append(a)
-LDA_related_DTM = sorted(LDA_related_DTM)
+            a = [j_index//TIME_SLICES, i_index]
+            if a not in DTM_related_LDA:
+                DTM_related_LDA.append(a)
+DTM_related_LDA = sorted(DTM_related_LDA)
 
 DTM_all, DTM_2orMore = DTMcount()
